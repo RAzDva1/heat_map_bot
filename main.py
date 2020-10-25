6075bf604c0b869fd884e6d69914ba36fa45a89a
RAzDva12 <valentin.goriachev@yandex.ru>
2020-10-25 23:13:36 +0300

    back to privious

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ def send_message_by_scheldier(chat_id):
     print("Schediler")
     bot.send_message(chat_id=chat_id, text="This message send you by scheduler")
 
-def print_hel(arh):
-    print("Hello, ", arh)
 
 sched = BackgroundScheduler(deamon=True)
-sched.add_job(print_hel, 'cron', args=[353688371], year='*', month='*',
+
+
+sched.add_job(send_message_by_scheldier, 'cron', args=[353688371], year='*', month='*',
               day='*', week='*', day_of_week='*',
               hour='*', minute='*', second=15)
 
-sched.add_job(print_hel, 'cron', args=[353688371], year='*', month='*',
+sched.add_job(send_message_by_scheldier, 'cron', args=[353688371], year='*', month='*',
               day='*', week='*', day_of_week='*',
               hour='*', minute='*', second=30)
 
-sched.add_job(print_hel, 'cron', args=[353688371], year='*', month='*',
+sched.add_job(send_message_by_scheldier, 'cron', args=[353688371], year='*', month='*',
               day='*', week='*', day_of_week='*',
               hour='*', minute='*', second=45)
 
@@ -38,7 +38,9 @@ sched.start()
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-
+    sched.add_job(send_message_by_scheldier, 'cron', args=[message.chai.id], year='*', month='*',
+                  day='*', week='*', day_of_week='*',
+                  hour='*', minute='*', second=50)
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
